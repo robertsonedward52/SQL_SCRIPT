0e463956d412388dd23649f0fa7d794ff573fe15
Edward Robertson <robertsonedward52@gmail.com>
2019-08-25 20:28:04 +0200

    I am commenting this file

---
+++ Pratice.sql
@@ -0,0 +1,52 @@
+/* SQL FOREIGN KEY Constraint
+  A FOREIGN KEY is a key used to link two tables together. */
+  
+  /* The "PersonID" column in the "Persons" table is the PRIMARY KEY in the "Persons" table.
+The "PersonID" column in the "Orders" table is a FOREIGN KEY in the "Orders" table.
+The FOREIGN KEY constraint is used to prevent actions that would destroy links between tables. */
+
+create database Tutorial;
+Use Tutorial;
+
+create table Person
+(Person_ID int primary key not null,
+Person_Name varchar (100) not null, 
+Person_Age int
+);
+
+create table Orders
+(Order_ID int not null,
+Order_Name varchar (100) not null, 
+Order_Date varchar (100) not null,
+Person_ID int,
+Primary Key (Order_ID),
+Foreign Key (Person_ID) References Person (Person_ID)
+);
+
+/* To allow naming of a FOREIGN KEY constraint, and for defining 
+a FOREIGN KEY constraint on multiple columns, use the following SQL syntax: */
+
+create table Employees
+(Employee_ID int not null,
+Employee_Name varchar (100) primary key not  null,
+Employee_Address varchar (100) not  null);
+
+
+create table Salaries
+(Salary_ID int not null,
+Salary_Amount float,
+Primary Key (Salary_ID),
+Employee_Name varchar (100),
+Constraint FK_EmployeesSalaries Foreign Key (Employee_Name) References Employees 
+(Employee_Name)
+);
+
+/* 
+DROP a FOREIGN KEY Constraint
+To drop a FOREIGN KEY constraint, use the following SQL: 
+*/
+Alter Table Salaries 
+Drop Foreign Key FK_EmployeesSalaries;
+
+
+
